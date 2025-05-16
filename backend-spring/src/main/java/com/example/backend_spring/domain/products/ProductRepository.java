00d7c28b7d9eeed9abb77668ef304cd4d9ffa89c
Fábio Alves <fbio.alves095@gmail.com>
2025-05-16 00:13:42 -0300

    del(products): removed products example domain

--- backend-spring/src/main/java/com/example/backend_spring/domain/products/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend_spring.domain.products;
-
-import java.util.UUID;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, UUID> {
-}
